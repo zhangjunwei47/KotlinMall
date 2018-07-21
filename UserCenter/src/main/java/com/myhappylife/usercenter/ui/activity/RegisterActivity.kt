2b58261eed8d62e760e074c5ab44fc7e5f5cfe07
zhangchao <zhangchao1@kaolafm.com>
2018-07-21 15:37:24 +0800

    绑定view 工具

--- UserCenter/src/main/java/com/myhappylife/usercenter/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/myhappylife/usercenter/ui/activity/RegisterActivity.kt
@@ -2,12 +2,17 @@ package com.myhappylife.usercenter.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.myhappylife.usercenter.R
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        mText.setOnClickListener{
+            Toast.makeText(this, "hahahaha", Toast.LENGTH_LONG).show()
+        }
     }
 }
