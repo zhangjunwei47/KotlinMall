52c3403878cf6a083746bfb9bba4ddff43409931
zhangchao <zhangchao1@kaolafm.com>
2018-07-21 15:27:36 +0800

    动态切换模块还是项目

---
+++ UserCenter/src/main/java/com/myhappylife/usercenter/ui/activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.myhappylife.usercenter.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.myhappylife.usercenter.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
